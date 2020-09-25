df4f8aed66797b6c5af69257f98094e517f448cb
simonminasse <66034261+simonminasse@users.noreply.github.com>
2020-09-25 12:35:22 -0400

    Plan for building RShiny App using CCVI dataset (originally downloaded 9/22/20) from Surgo Foundation.

---
+++ ccvi_intro.R
@@ -0,0 +1,6 @@
+# Project Plan
+# Organize CCVI state data into regions as defined by National Geographic
+# Create RShiny App that provides a bar chart of CCVI SCORE by Region
+# The RShiny App should also provide bar charts for each of the 6 themes
+# For each theme, the bar charts are based on median and average scores for the region
+
